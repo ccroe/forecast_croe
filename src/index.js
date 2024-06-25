bcf707fc81a7d4ea99a822da9b2bbfb7a6013139
ccroe <romerochrystal35@gmail.com>
2024-06-24 22:21:03 -0700

    html and css updated

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ function formatDay(timestamp) {
 }
 
 function getForecast(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "t134c4977ef73a2f9a6202bcb4ea1dob";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
